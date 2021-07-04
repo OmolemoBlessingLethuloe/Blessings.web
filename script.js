fab235966d52a4ee30fd68de92350554684fbab3
Omolemo Lethuloe <olethuloe@gmail.com>
2021-07-04 19:48:10 +0200

    added modal

---
+++ script.js
@@ -0,0 +1,29 @@
+// get the modal
+let modal = document.getElementById("myModal");
+
+// let image = document.getElementsByClassName("modal-click");
+
+// button that closes modal
+let span = document.getElementsByClassName("close")[0];
+
+
+function myFunction() {
+    let x = document.getElementsByClassName("modal");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// when the user clicks the (x) on the modal, close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// when the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
